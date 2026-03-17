26b27abdeeb0e586769f6dcd9321d1c7f2be0a33
Eda95s <semenyugin5121995@gmail.com>
2026-03-17 20:52:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,5 @@ app.post('/api/click', async (req, res) => {
     }
 });
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
